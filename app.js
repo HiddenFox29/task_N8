76906fa9c4ccbfcbdf45394107159c4678802dd4
Aleksandr Alekseev <hiddenfox29@yandex.ru>
2019-10-13 22:30:19 +0600

    first

---
+++ app.js
@@ -0,0 +1,24 @@
+let uk = document.getElementById("uk");
+let kz = document.getElementById("kz");
+let ru = document.getElementById("ru");
+
+uk.onclick = function () {
+    setTimeout(function() {
+        alert("Hello");
+    }, 1700);
+};
+
+kz.onclick = function () {
+    setTimeout(function() {
+        alert("Salem");
+    }, 1700);
+};
+
+ru.onclick = function () {
+    setTimeout(function() {
+        alert("Привет");
+    }, 1700);
+};
+
+
+
